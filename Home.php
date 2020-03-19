f59cf56c456cc483cda68f6bf2cd6bd0fc3e0b8a
nesma <nesmahmz@gmail.com>
2020-03-19 14:15:07 +0000

    home page

---
+++ Home.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css\s.css">
+	<link rel="stylesheet" type="text/css" href="css\navbar.css">
+</head>
+<body>
+    <div class="box1">
+    <h2 class="box">WELCOME TO EGYPT E-Visa PORTAL</h2>
+  </div>
+ <ul>
+  <li><a class="active" href="login_admin.php">Admin</a></li>
+  <li><a href="login_user.php">Users</a></li>
+</ul>
+</body>
+</html>
